d9db2d89c363e6484d178590d67e19a07b928af4
Vovk Eugene <zekavovk@yandex.ru>
2018-09-10 16:53:15 +0300

    moving in folders

--- FRead.js
+++ FRead.js
@@ -1,39 +1,89 @@
-const fs = require('fs');
-const path = require('path');
-const directoryPath = path.join(__dirname, 'test');
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
 
-fs.readdir(directoryPath, (err, files) => {
-	if (err) {
-		throw err;
-	}
-	files.forEach(function (file) {
-		var fileName = file.split('.')[0]
-		currentRead('./test/' + file, fileName);
-	});
-});
-
-function currentRead(file, fileName) {
-	fs.readFile(file, (err, data) => {
-		if(err) {
-			throw err;
-		}
-
-		var array = data.toString().split('\n');
-		var obj = {
-			fileName: []
-		};
-		for(i in array) {
-			obj.fileName.push(array[i]);
-		}
-		var json = JSON.stringify(obj, null, '  ').replace(/[\\n\\r]/g, '');
-		currentWrite(json);
-	});
+if (process.argv.length < 3) {
+  console.error('err:', 'third argument(pattern) is required')
+  process.exit(1)
 }
 
-function currentWrite(data) {
-	fs.writeFile('test.json', data, 'utf8', (err) => {
-		if (err) {
-			throw err;
-		}
-	})
+// const pattern = process.argv[2] // '**/*.txt'
+const dirPath = process.argv[3] || process.cwd()
+const pattern = '**/*.txt'
+console.log('pattern:', pattern)
+
+const filesName = 'files.json'
+const resultName = 'result.json'
+
+const options = {
+  cwd: dirPath,
+  ignore: '**/node_modules/**'
+}
+
+find(pattern, options)
+  .then(function (files) {
+    console.log('files:', files)
+
+    fs.writeFileSync(filesName, JSON.stringify(files, 0, 1))
+
+    const promises = files.map(function (file) {
+      const filePath = path.join(dirPath, file).replace(/\\/g, "/")
+      return readFile(filePath, {}).then(function (content) {
+        return {
+          filePath: filePath,
+          content: content.toString(),
+        }
+      })
+    })
+
+    return Promise.all(promises).then(function (data) {
+      const t = data.reduce(function (acc, item) {
+        if (!Array.isArray(acc[item.filePath])) {
+          acc[item.filePath] = []
+        }
+        const array = item.content.split(/\r?\n/).filter(item => item)
+        acc[item.filePath] = acc[item.filePath].concat(array)
+
+        return acc
+      }, {})
+
+      console.log('contents:', t)
+
+      fs.writeFileSync(resultName, JSON.stringify(t, 0, 1))
+    })
+  })
+  .catch(function (err) {
+    console.error('err:', err)
+  })
+
+/**
+ * @param {String} pattern
+ * @param {Object} [options]
+ * @returns {Promise<*>}
+ */
+function find(pattern, options) {
+  return new Promise(function (resolve, reject) {
+    glob(pattern, options, function (err, files) {
+      if (err) {
+        return reject(err)
+      }
+      resolve(files)
+    })
+  })
+}
+
+/**
+ * @param {String} path
+ * @param {Object} [options]
+ * @returns {Promise<*>}
+ */
+function readFile(path, options) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(path, options, function (err, data) {
+      if (err) {
+        return reject(err)
+      }
+      resolve(data)
+    })
+  })
 }
--- config/index.js
+++ config/index.js
@@ -1,55 +1,55 @@
 module.exports = {
-  'watchers': {
-    'scripts': ['js/**/*.js', '!js/external/**/*.js'],
-    'styles': ['css/**/*.css', '!css/external/**/*.css']
-  },
-  'assets': {
-    'general': {
-      'js': [
-        'js/some.js'
-      ],
-      'css': [
-        'css/some.css'
-      ]
-    }
-    // 'page1': {
-    //   'js': [
-    //     'js/some_1.js',
-    //     'js/some_2.js'
-    //   ],
-    //   'css': [
-    //     'css/some_1.css',
-    //     'css/some_2.css'
-    //   ]
-    // },
-    // 'page2': {
-    //   'js': [
-    //     'js/some_2.js',
-    //     'js/some_3.js',
-    //     'js/some_4.js'
-    //   ],
-    //   'css': [
-    //     'css/some_2.css',
-    //     'css/some_3.css'
-    //   ]
-    // },
-    // 'page3': {
-    //   'js': [
-    //     'js/some_3.js'
-    //   ],
-    //   'css': [
-    //     'css/some_3.css'
-    //   ]
-    // },
-    // 'page4': {
-    //   'js': [
-    //     'js/some_2.js',
-    //     'js/some_4.js'
-    //   ],
-    //   'css': [
-    //     'css/some_2.css',
-    //     'css/some_4.css'
-    //   ]
-    // }
-  }
+	'watchers': {
+		'scripts': ['js/**/*.js', '!js/external/**/*.js'],
+		'styles': ['css/**/*.css', '!css/external/**/*.css']
+	},
+	'assets': {
+		'general': {
+			'js': [
+			'js/some.js'
+			],
+			'css': [
+			'css/some.css'
+			]
+		}
+		// 'page1': {
+		// 	'js': [
+		// 	'js/some_1.js',
+		// 	'js/some_2.js'
+		// 	],
+		// 	'css': [
+		// 	'css/some_1.css',
+		// 	'css/some_2.css'
+		// 	]
+		// },
+		// 'page2': {
+		// 	'js': [
+		// 	'js/some_2.js',
+		// 	'js/some_3.js',
+		// 	'js/some_4.js'
+		// 	],
+		// 	'css': [
+		// 	'css/some_2.css',
+		// 	'css/some_3.css'
+		// 	]
+		// },
+		// 'page3': {
+		// 	'js': [
+		// 	'js/some_3.js'
+		// 	],
+		// 	'css': [
+		// 	'css/some_3.css'
+		// 	]
+		// },
+		// 'page4': {
+		// 	'js': [
+		// 	'js/some_2.js',
+		// 	'js/some_4.js'
+		// 	],
+		// 	'css': [
+		// 	'css/some_2.css',
+		// 	'css/some_4.css'
+		// 	]
+		// }
+	}
 };
